c6319fcb48bfaf254d5448c3b98ac622f106b20e
gavand-sagar <gavand.sagar.s@gmail.com>
2023-09-30 11:40:19 +0530

    cleaned up

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,7 @@
 public class Main {
         public static void main(String[] args) {
 
-                Object[] items = new Object[5];
-                items[0] = "A";
-                items[1] = "B";
-                items[2] = "C";
-                items[3] = "D";
-                items[4] = "E";
 
-
-
-
-                MyIterator iterator = new EverySecondItemCollection(items);
-                while (iterator.hasNext()){
-                        Object o = iterator.next();
-                        System.out.println(o);
-                }
 
 
 
