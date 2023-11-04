cb7b2bcb77113dbd147a197fc79b0957209539c6
gavand-sagar <gavand.sagar.s@gmail.com>
2023-11-04 10:52:00 +0530

    tail recursion

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,39 @@
 public class Main {
         public static void main(String[] args) {
+            System.out.println(factorialTail(5,1));
+        }
 
+        //non tail
+        int factorial(int number){
+            if(number<=1){
+                return  1;
+            }
+            else{
+                return number * factorial(number - 1);
+            }
+        }
 
+        //tail
+        static  int factorialTail(int number,int out){
+            if(number<=1){
+                return  out;
+            }
+            out = out * number;
+            return factorialTail(number-1, out);
+        }
 
+        public static void fun1(int number){
+            System.out.println(number);
+            if(number <10){
+                fun2(number+1);
+            }
+        }
+        public static void fun2(int number){
+            System.out.println(number);
+            if(number <10){
+                fun1(number+1);
+            }
 
+        }
 
-
-
-    }
 }
