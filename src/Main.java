38b8f964c590eeb7748b3673691eb2eec83adeb7
gavand-sagar <gavand.sagar.s@gmail.com>
2023-12-09 10:31:46 +0530

    basic int stack - with exceptions

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,14 @@ import javax.swing.*;
 
 public class Main {
       private  static  IStack stack = new Stack();
-      public static void main(String[] args) {
+      public static void main(String[] args) throws Exception {
 
             System.out.println(stack.isEmpty()); // true;
 
             stack.push(4);
             stack.push(5);
             stack.push(6);
+
 
             System.out.println(stack.peek());  // 6
             System.out.println(stack.peek());  // 6
