de04ce97db1a8824349491e0119ed480c2a5f9a0
gavand-sagar <gavand.sagar.s@gmail.com>
2023-12-16 09:49:51 +0530

    stack - reverse string

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,26 @@
+import java.util.Stack;
+
 public class Main {
         public static void main(String[] args) {
+            // Reverse of string using stack
+            String name = "Sagar";
+            String reverseOfName = reverse(name);
+            System.out.println(reverseOfName);
+            // r    a   g   a   S
+        }
 
+        public  static  String reverse(String input){
+            StringBuilder output = new StringBuilder();
+            Stack<Character> stack = new Stack<Character>();
+            // stack  insert one by one      S   a   g   a   r
+            for (int i = 0; i < input.length(); i++) {
+                stack.push(input.charAt(i));
+            }
+            // keep removing item one by one;
+            while (!stack.isEmpty()){
+                output.append(stack.pop());
+            }
+            return output.toString();
+        }
 
-
-
-
-
-
-    }
 }
