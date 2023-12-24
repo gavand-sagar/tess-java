38633cc570a38f68ebd39615e99ec0f47f3f0f0e
gavand-sagar <gavand.sagar.s@gmail.com>
2023-12-24 11:41:46 +0530

    get unique collection

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,27 @@
+import java.util.*;
+
 public class Main {
         public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            Set<String> items = new HashSet<>();
+
+            System.out.println("Enter 2 phone numbers");
+            while (items.size()<2){
+                System.out.println("Enter Phone number");
+                if(!items.add(scanner.nextLine())){
+                    System.out.println("please enter new data");
+                }
+            }
+
+            System.out.println("Thank you for entering the details");
+            for (String s:items) {
+                System.out.print(s+"\t");
+            }
 
 
 
-
-
-
+            //input --> 56 78 90 56 70
+            // output --> 56 78 90 70
 
     }
 }
