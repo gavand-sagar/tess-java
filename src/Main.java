416a6166ff4ecaf1d51088dd8a5c43d1497d9bdc
gavand-sagar <gavand.sagar.s@gmail.com>
2023-12-24 11:49:28 +0530

    remove duplicates

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,20 @@
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
 public class Main {
         public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            Set<Integer> set = new HashSet<>();
+            while (true){
+                System.out.println("Enter a number");
+                int number = scanner.nextInt();
+                if(number < 0){
+                    break;
+                }
+                set.add(number);
+            }
 
-
-
-
-
-
+            System.out.println(set);
 
     }
 }
