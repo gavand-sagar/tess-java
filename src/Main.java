c788e01ed8e8363325d242c833e5e961a2390386
gavand-sagar <gavand.sagar.s@gmail.com>
2024-01-27 10:54:05 +0530

    selection sort

--- src/Main.java
+++ src/Main.java
@@ -4,34 +4,33 @@ import java.util.Map;
 public class Main {
         public static void main(String[] args) {
 
-            System.out.println("Calculated -" + getSquareButSmartly(3));
-            System.out.println("Calculated -" + getSquareButSmartly(4));
-            System.out.println("Calculated -" + getSquareButSmartly(5));
-            System.out.println("Calculated -" + getSquareButSmartly(3));
-            System.out.println("Calculated -" + getSquareButSmartly(3));
+            int[] array = new int[] {9,12,3,4,5,6,7,8};
 
-        }
+            for (int i = 0; i < array.length - 1; i++) {
+                int currentNumber = array[i];
 
-        public  static Map<Integer,Integer> results = new HashMap<Integer,Integer>();
-        public static int getSquareButSmartly(int number){
-            if(results.containsKey(number)){
-                return results.get(number);
-            }else{
-                int answer = getSquare(number);
-                results.put(number,answer);
-                return  answer;
-            }
-        }
+                int lowest = i + 1;
+                for (int j = i +1 ; j < array.length; j++) {
+                    if(array[j]<array[lowest]){
+                        lowest = j;
+                    }
+                }
 
+                if(currentNumber > array[lowest]){
+                    array[i] = array[lowest];
+                    array[lowest] = currentNumber;
+                }
 
-        public static int getSquare(int number){
-            System.out.println("Now Calculating for "+ number);
-            try{
-                Thread.sleep(1500);
-            }catch (Exception e){
 
             }
-            return number * number;
+
+
+            for (int number:array ) {
+                System.out.print(number+"\t");
+            }
+
         }
+
+
 
 }
