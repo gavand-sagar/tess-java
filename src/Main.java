81f6464c35e49dac2c037c9ebe40bcc2167cf165
gavand-sagar <gavand.sagar.s@gmail.com>
2024-01-28 11:54:52 +0530

    base code

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,39 @@ import java.util.Map;
 public class Main {
         public static void main(String[] args) {
 
+                int[] array = new int[] {5,3,1,7,2,9,6};
+                System.out.println("Before");
+                for (int number:array) {
+                        System.out.print( number + "\t");
+                }
+                System.out.println();
+                System.out.println("After");
+                int[] result = quickSort(array);
+                for (int number:result) {
+                        System.out.print( number + "\t");
+                }
+        }
 
+        private static int[] quickSort(int[] array) {
+                if(array.length <= 1){
+                    return array;
+                }
+                int mid = getMid(array);
+                int[] leftArray = getLowestItems(array,mid);
+                int[] rightArray = getGreaterItems(array,mid);
+                return concat(quickSort(leftArray),new int[]{mid},quickSort(rightArray));
+        }
+
+        private static int[] concat(int[] leftArray, int[] ints, int[] rightArray) {
+        }
+
+        private static int[] getGreaterItems(int[] array, int mid) {
+        }
+
+        private static int[] getLowestItems(int[] array, int mid) {
+        }
+
+        private static int getMid(int[] array) {
 
         }
 
