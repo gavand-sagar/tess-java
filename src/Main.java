fbfb16de59701d946d18f843d6959dbd37459118
gavand-sagar <gavand.sagar.s@gmail.com>
2024-02-03 10:45:53 +0530

    implementation

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,46 @@
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Main {
         public static void main(String[] args) {
 
+                List<Person> people = new ArrayList<Person>();
+                people.add(new Person(1,"Shrikrishna"));
+                people.add(new Person(2,"Pavan"));
+                people.add(new Person(3,"Swati"));
+                people.add(new Person(4,"Vinitha"));
+
+
+                if(search(people,"Shrikrishna")){
+                        System.out.println("Found");
+                }else{
+                        System.out.println("Not found");
+                }
+
 
 
         }
+        public static boolean search(int[] array, int itemToFind){
+                boolean flag = false;
+                for (int element : array) {
+                        if (element == itemToFind) {
+                                flag = true;
+                                break;
+                        }
+                }
+                return flag;
+        }
+
+        public static boolean search(List<Person> list, String name){
+                for (Person p: list) {
+                        if(p.name.equals(name)){
+                                return true;
+                        }
+                }
+                return false;
+        }
+
 
 }
---
+++ src/Person.java
@@ -0,0 +1,12 @@
+public class Person {
+    public int id;
+    public String name;
+
+    public Person(int id,String name){
+        this.id = id;
+        this.name = name;
+    }
+
+
+
+}
