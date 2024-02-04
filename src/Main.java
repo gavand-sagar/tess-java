965ce7015253a5cb5399d047b532a20b62907e1f
gavand-sagar <gavand.sagar.s@gmail.com>
2024-02-04 09:14:36 +0530

    linear search

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,26 @@ import java.util.Map;
 public class Main {
         public static void main(String[] args) {
 
+                int[] array = new int[]{7,6,3,9,10,2,4,5};
+
+                if(search(array,1)){
+                        System.out.println("Found");
+                }else{
+                        System.out.println("Not found");
+                }
 
 
         }
+
+        public static boolean search(int[] array,int value){
+                for (int item: array) {
+                        if(item == value){
+                                return  true;
+                        }
+                }
+                return  false;
+
+        }
+
 
 }
