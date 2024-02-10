89f59c3f5c4e56a37467b0afcdc68a3a770abd50
gavand-sagar <gavand.sagar.s@gmail.com>
2024-02-10 11:27:00 +0530

    memoized

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,36 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Main {
-        public static void main(String[] args) {
 
+        static Map<Integer,Integer> results = new HashMap<>();
+        public static void main(String[] args) throws InterruptedException {
 
+                System.out.println(sumFrom1ToN(5));
+                System.out.println(sumFrom1ToN(5));
+                System.out.println(sumFrom1ToN(5));
+
+                System.out.println(sumFrom1ToN(6));
+                System.out.println(sumFrom1ToN(7));
 
         }
+
+        public  static  int sumFrom1ToN(int input) throws InterruptedException {
+                if(results.containsKey(input)){
+                       return  results.get(input);
+                };
+
+
+                if(input == 1 ){
+                        results.put(input,1);
+                        return 1;
+                }
+
+                Thread.sleep(1000);
+                int output = input + sumFrom1ToN(input - 1);
+                results.put(input,output);
+                return output;
+        }
+
 
 
 }
