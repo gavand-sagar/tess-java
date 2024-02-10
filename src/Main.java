1d04ed812e4acb5bafd36a0147bde7b8a92d049e
gavand-sagar <gavand.sagar.s@gmail.com>
2024-02-10 10:11:42 +0530

    cleared

--- src/Main.java
+++ src/Main.java
@@ -4,63 +4,8 @@ import java.util.Map;
 public class Main {
         public static void main(String[] args) {
 
-                int[] array = new int[]{3,5,6,7,8,9,10};
-
-                if(binarySearch(array,17)){
-                        System.out.println("Found");
-                }else{
-                        System.out.println("Not found");
-                }
-
-        }
 
 
-        public  static  boolean binarySearch(int[] array,int value){
-                if(array.length == 0){
-                        return  false;
-                }
-                if(array.length == 1){
-                        return (array[0] == value);
-                }
-
-                int midValue = getMidValue(array);
-
-                if(midValue == value){
-                        return true;
-                }else if(value < midValue){
-                        int[] left = getLeft(array);
-                        return binarySearch(left,value);
-                }else{ // value > midValue
-                        int[] right = getRight(array);
-                        return binarySearch(right,value);
-                }
-
-        }
-
-        private static int[] getRight(int[] array) {
-                int midIndex = array.length / 2;
-                int newLength = array.length - (midIndex +1);
-                int[] right = new int[newLength];
-                int c = 0;
-                for (int i = midIndex + 1 ; i < array.length; i++) {
-                        right[c] = array[i];
-                        c++;
-                }
-                return right;
-        }
-
-        private static int[] getLeft(int[] array) {
-                int midIndex = array.length / 2;
-                int[] left = new int[midIndex];
-                for (int i = 0; i < midIndex; i++) {
-                        left[i] = array[i];
-                }
-                return left;
-        }
-
-        private static int getMidValue(int[] array) {
-                int midIndex = array.length / 2;
-                return array[midIndex];
         }
 
 
