a927d1bef7305e3e70a8ea34e7bdaf1c47cdf285
gavand-sagar <gavand.sagar.s@gmail.com>
2024-02-11 11:14:23 +0530

    optimized

--- src/Main.java
+++ src/Main.java
@@ -3,17 +3,36 @@ import java.util.Map;
 
 public class Main {
 
+        public  static  int counter = 0;
+        public static Map<String,Boolean> memo = new HashMap<>();
         public static boolean isPalindrome(String input) throws InterruptedException {
+
+                if(memo.containsKey(input)){
+                        return memo.get(input);
+                }
+
+
+                counter++;
                 StringBuilder reverse = new StringBuilder();
                 for (int i = input.length() - 1 ; i >= 0; i--) {
                         reverse.append(input.charAt(i));
                 }
-                return reverse.toString().equals(input);
+                boolean result = reverse.toString().equals(input);
+                memo.put(input,result);
+                if(result){
+                        while (input.length() > 2){
+                                input = input.substring(1, input.length() - 1);
+                                memo.put(input,true);
+                        }
+                }
+
+                return result ;
         }
 
         public static void main(String[] args) throws InterruptedException {
 
                 String str = "malayalam";
+
 
                 for (int j = 0; j < str.length(); j++) {
                         String temp = "";
@@ -25,7 +44,7 @@ public class Main {
                         }
                 }
 
-
+                System.out.println("\ncounter- " + counter);
         }
 
 
