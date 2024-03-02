fa6f93af69548d3551dd81c20f1718e97b073f07
gavand-sagar <gavand.sagar.s@gmail.com>
2024-03-02 12:28:01 +0530

    minimum platforms

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,31 @@ import java.util.Map;
 public class Main {
         public static void main(String[] args) {
 
+                int[] a = new int[]{900,915,930,945,1200};
+                int[] d = new int[]{930,930,1000,1030,1230};
+
+                int count = 0;
+                int ans = 0;
+
+                int i = 0, j = 0;
+
+                while (i< a.length){
+                       if(a[i] <= d[j]){
+                               count++;
+                               ans = Math.max(ans,count);
+                               i++;
+                       }else{
+                               count--;
+                               j++;
+                       }
+
+                }
+
+                System.out.println(ans);
+
+
+
+
         }
 
 
