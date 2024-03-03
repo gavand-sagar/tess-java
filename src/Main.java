d0d3b97f3b85598116985e91c9b9ff551fcf7393
gavand-sagar <gavand.sagar.s@gmail.com>
2024-03-03 11:40:15 +0530

    stocks max profit - multiple buy

--- src/Main.java
+++ src/Main.java
@@ -3,36 +3,22 @@ import java.util.Map;
 
 public class Main {
         public static void main(String[] args) {
-                int[] priceLine = new int[]{30,10,20,60,20,70};
+
+                int[] priceLine = new int[]{40,20,30,10,20,60,20,70};
 
                 int profit = 0;
-                for (int i = 0; i < priceLine.length; i++) {
-                        for (int j = i+1; j < priceLine.length; j++) {
-                                int buy = priceLine[i];
-                                int sell = priceLine[j];
 
-                                int p = sell - buy;
-                                if(profit <= p){
-                                        profit = p;
-                                }
+                for (int i = 0; i < priceLine.length-1; i++) {
+                        int current = priceLine[i];
+                        int next = priceLine[i+1];
+                        if(next>current){
+                                System.out.println(current+","+next);
+                                profit += next-current;
+                        }
 
-                         }
                 }
-                System.out.println("profit : " + profit);
 
-
-//                int temp = 0;
-//                for (int i = 0; i < priceLine.length - 1; i++) {
-//                        int next = priceLine[i+1];
-//                        int current = priceLine[i];
-//                        int profit = next-current;
-//                        if(profit>temp){
-//                                temp = profit;
-//                        }
-//                }
-//
-//                System.out.println(temp);
-
+                System.out.println("profit: " + profit);
 
 
         }
