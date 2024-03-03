44e0c79da80584fc14731c6366d0fc19cf832d85
gavand-sagar <gavand.sagar.s@gmail.com>
2024-03-03 12:35:30 +0530

    total cost of candies

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,30 @@
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Main {
         public static void main(String[] args) {
 
+
+                int[] candies = new int[] {1,2,5,10,30,60,30,30,5,10,10,20};
+                sortArray(candies);
+
+                int totalCost = 0;
+
+                for (int i = candies.length-1; i >= 0; i-=3) {
+                    totalCost += candies[i];
+                    if(i>0){
+                        totalCost += candies[i-1];
+                    }
+                }
+
+                System.out.println(totalCost);
+
         }
+
+    private static void sortArray(int[] candies) {
+            Arrays.sort(candies);
+    }
 
 
 }
