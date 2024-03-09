0d615d8673846ba8f1115e07d62861cf1254869d
gavand-sagar <gavand.sagar.s@gmail.com>
2024-03-09 09:58:42 +0530

    total cost of candies

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,22 @@ import java.util.Map;
 public class Main {
         public static void main(String[] args) {
 
+                int[] numbers = new int[]{1,3,5,6,9,10,50,70,71};;
+                        //display all numbers
+
+                for (int i = 0; i < numbers.length - 1; i++) {
+                        int current = numbers[i];
+                        int next = numbers[i+1];
+                        if(current + 1 == next){
+                                System.out.println(current + "," + next);
+                        }
+                        
+                }
+
+                        // dipslay current and next
+
+
+                        // display only when its n the sequemce
         }
 
 
